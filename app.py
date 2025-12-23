5073491ff1181f4125c3a2efe48a57cc3b1f1053
Akshay Raikar <xsabber5@gmail.com>
2025-12-23 11:11:35 +0530

    Initial version of PrepMate AI

---
+++ app.py
@@ -0,0 +1,38 @@
+from flask import Flask, render_template, request
+import google.generativeai as genai
+
+app = Flask(__name__)
+
+# Configure Gemini
+genai.configure(api_key="AIzaSyDxqdTYmkHWhT1UdQYj9bE15xA4sv0uXss")
+
+model = genai.GenerativeModel("gemini-2.5-flash")
+
+@app.route("/", methods=["GET", "POST"])
+def home():
+    question = ""
+    if request.method == "POST":
+        role = request.form["role"]
+        level = request.form["level"]
+
+        prompt = f"""
+            Generate a {level} React interview question.
+            Format strictly like this:
+
+            Question:
+            (one line)
+
+            Expected Answer:
+            (3–4 short points)
+
+            Key Concepts:
+            (2–3 keywords only)
+            """
+
+        response = model.generate_content(prompt)
+        question = response.text
+
+    return render_template("index.html", question=question)
+
+if __name__ == "__main__":
+    app.run(debug=True)
