0f29a96a6dbd85a34aaf0f0079751d159009d011
Akshay Raikar <xsabber5@gmail.com>
2025-12-24 13:38:32 +0530

    changing the flask port to 0.0.0.0

--- app.py
+++ app.py
@@ -36,4 +36,4 @@ def home():
     return render_template("index.html", question=question)
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 10000)))
